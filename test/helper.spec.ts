4dcb87af6ead106c12c19c7e34419b107e125c99
mikrowdev <98613824+mikrowdev@users.noreply.github.com>
2025-07-20 17:51:45 +0200

    Feat/implement mask secret fn (#33)
    * chore: bump version to 0.9.2
    
    * feat: implemented maskSecret function
    
    * docs: update release notes for version 0.9.2

--- test/helper.spec.ts
+++ test/helper.spec.ts
@@ -1,4 +1,5 @@
 import { expect } from './testlib';
+import { maskSecret } from '@fp8proj/helper';
 
 import {
     isEmpty, localDebug
@@ -25,5 +26,32 @@ describe('helper', () => {
     it('test debug -- Should not throw error', () => {
         const data = '{"bad":12';
         localDebug(() => `Test: ${JSON.stringify(JSON.parse(data))}`);
+    });
+
+    it('maskSecret less than 12 char', () => {
+        const secret = 'secret';
+        const res = maskSecret(secret);
+        
+        const expected = 'K7gN...qoVW';
+        
+        expect(res).to.equal(expected);
+    });
+
+    it('maskSecret with force enabled', () => {
+        const secret = 'secret-length-more-than-twelve-char';
+        const res = maskSecret(secret,true);
+        
+        const expected = '5jQt...RclM';
+        
+        expect(res).to.equal(expected);
+    });
+
+    it('maskSecret more than 12 char', () => {
+        const secret = 'secret-twelve';
+        const res = maskSecret(secret);
+        
+        const expected = 'secr...elve';
+        
+        expect(res).to.equal(expected);
     });
 });
